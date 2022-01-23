155ad0edda5d3d62ff6cf641293776bce1ee660b
aarni <aarni.alasaarela@gmail.com>
2022-01-24 00:54:31 +0200

    fix init_logging module name

--- upkeep/log.py
+++ upkeep/log.py
@@ -8,7 +8,7 @@ def init_logging(logger_name):
         os.makedirs('logs/')
     session_start = time.strftime('%Y%m%dT%H%M%S')
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    log_file = 'logs/crawler' + session_start + '.log'
+    log_file = 'logs/upkeep' + session_start + '.log'
 
     file_handler = logging.FileHandler(log_file)
     file_handler.setFormatter(formatter)
@@ -21,7 +21,7 @@ def init_logging(logger_name):
     root_logger.addHandler(file_handler)
     root_logger.addHandler(console_handler)
 
-    logging.getLogger('crawler').setLevel(logging.INFO)
+    logging.getLogger('upkeep').setLevel(logging.INFO)
 
     logger = logging.getLogger(logger_name)
     logger.setLevel(logging.INFO)
